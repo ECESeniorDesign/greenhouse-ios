e3cbbdf8846306100efcbe0fe2f380864eebde32
Chase Conklin <chase@conklins.net>
2016-03-13 16:52:20 -0400

    Rename root controller

--- Greenhouse/RootViewController.swift
+++ Greenhouse/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Greenhouse
 //
 //  Created by Chase Conklin on 3/10/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

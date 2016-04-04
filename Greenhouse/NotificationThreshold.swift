459021103f1f8704cb4ef58b3015868735db012a
Chase Conklin <chase@conklins.net>
2016-04-04 15:28:54 -0400

    Added table to show notification settings

---
+++ Greenhouse/NotificationThreshold.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationThreshold.swift
+//  Greenhouse
+//
+//  Created by Chase Conklin on 4/4/16.
+//  Copyright © 2016 ECESeniorDesign. All rights reserved.
+//
+
+import Foundation
+
+class NotificationThreshold : NSObject {
+
+    var name : String
+    var id : Int
+    var deviationTime : Float
+    var deviationPercent : Int
+    
+    init(name: String, id: Int, deviationTime: Float, deviationPercent: Int) {
+        self.name = name
+        self.id = id
+        self.deviationTime = deviationTime
+        self.deviationPercent = deviationPercent
+    }
+    
+}
--- Greenhouse/PlantSettingViewController.swift
+++ Greenhouse/PlantSettingViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class PlantSettingViewController: UIViewController {
+class PlantSettingViewController: UIViewController, APIRequestDelegate {
+    @IBOutlet weak var thresholdsTable: UITableView!
+    
+    var notificationThresholds : [String: [NotificationThreshold]] = [:]
+    var thresholdNames : [String] = []
+    
     @IBAction func cancelButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
@@ -25,13 +31,60 @@ class PlantSettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let apiRequest = APIRequest(urlString: "http://\(Config.greenhouse)/api/plants/\(plant!.slotID!)/settings")
+        apiRequest.sendGETRequest(self)
         // Do any additional setup after loading the view.
     }
 
+    func handleData(data: NSData!) {
+        if data != nil {
+            let json = JSON(data: data)
+            let settings = json["settings"].array!
+            for setting in settings {
+                let notificationThreshold = NotificationThreshold(name: setting["sensor_name"].string!, id: setting["id"].int!, deviationTime: setting["deviation_time"].float!, deviationPercent: setting["deviation_percent"].int!)
+                if notificationThresholds[notificationThreshold.name] != nil {
+                    notificationThresholds[notificationThreshold.name]!.append(notificationThreshold)
+                } else {
+                    notificationThresholds[notificationThreshold.name] = [notificationThreshold]
+                    thresholdNames.append(notificationThreshold.name)
+                }
+            }
+            dispatch_async(dispatch_get_main_queue(), {
+                self.thresholdsTable.reloadData()
+            })
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    internal func tableView( tableView : UITableView,  titleForHeaderInSection section: Int) -> String {
+        return thresholdNames[section]
+    }
+    
+    internal func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return thresholdNames.count
+    }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notificationThresholds[thresholdNames[section]]!.count
+    }
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("notificationThresholdCell")!
+        let section = notificationThresholds[thresholdNames[indexPath.section]]!
+        let threshold = section[indexPath.row]
+        cell.textLabel?.text = "\(threshold.deviationPercent)% for \(threshold.deviationTime) hours"
+        return cell
+    }
+    
+    internal func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let row = indexPath.row
+        print("Row selected: \(row)")
+    }
+
     
     /*
     // MARK: - Navigation
